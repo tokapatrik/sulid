cb2307cfc4b0d4e415e565f00ec7d16d12209f5e
tokapatrik <tokapatrik321@gmail.com>
2022-11-15 13:33:41 +0100

    belső oldal voltozás, menüság

--- priv_files/js/private.js
+++ priv_files/js/private.js
@@ -1,11 +1,52 @@
-  $(".menubarButton").mouseenter(function () {
+  $(".menubarButtonMain, .menubarButtonSub").mouseenter(function () {
     $(this).css("color", "#fff");
     $(this).css("background-color", "#0000004d");
     $(this).children('a').css("color", "#fff");
   });
 
-  $(".menubarButton").mouseleave(function () {
+  $(".menubarButtonMain, .menubarButtonSub").mouseleave(function () {
     $(this).css("color", "#6c6c6c");
     $(this).css("background", "transparent");
     $(this).children('a').css("color", "#6c6c6c");
   });
+
+  $("#mainIskola").click(function(){
+    closeAll("#subIskola");
+    $("#subIskola").slideToggle(400);
+  });
+  $("#mainTanarok").click(function(){
+    closeAll("#subTanarok");
+    $("#subTanarok").slideToggle(400);
+  });
+  $("#mainTanulok").click(function(){
+    closeAll("#subTanulok");
+    $("#subTanulok").slideToggle(400);
+  });
+  $("#mainVaezetoseg").click(function(){
+    closeAll("#subVaezetoseg");
+    $("#subVaezetoseg").slideToggle(400);
+  });
+
+  function closeAll(objString)
+  {
+    if ($("#subIskola").is(":visible")) {
+      if (objString!="#subIskola") {
+        $("#subIskola").slideToggle(400);
+      }
+    }
+    if ($("#subTanarok").is(":visible")) {
+      if (objString!="#subTanarok") {
+        $("#subTanarok").slideToggle(400);
+      }
+    }
+    if ($("#subTanulok").is(":visible")) {
+      if (objString!="#subTanulok") {
+        $("#subTanulok").slideToggle(400);
+      }
+    }
+    if ($("#subVaezetoseg").is(":visible")) {
+      if (objString!="#subVaezetoseg") {
+        $("#subVaezetoseg").slideToggle(400);
+      }
+    }
+  }
--- priv_files/priv-site-top.php
+++ priv_files/priv-site-top.php
@@ -25,42 +25,158 @@ $userClassPtr->userLogdIn();
 
 <body style="margin: 0;padding: 0;height: 100%;max-height: 100%;float: left;width: 100%;">
     <div class="row p-0 m-0 h-100">
-        <div class="col-2 m-0 p-0 position-relative menubar" style="width: 13%;background: #282f3d;">
+        <div class="col-2 m-0 p-0 position-relative menubar" style="background: #282f3d;">
             <div class="nameBox m-3 pb-3">
                 <?echo '<div class="nameBoxLogo m-0 p-0" style="background: #'.$misc->stringToColorCode($_SESSION["user"]["usr_nev"]).';">'.substr($_SESSION["userTipusAdatok"][$_SESSION["user"]["usr_tipus"]."_nev_vezetek"],0,1).substr($_SESSION["userTipusAdatok"][$_SESSION["user"]["usr_tipus"]."_nev_kereszt"],0,1).'</div>';?>
                 <?echo '<div class="ms-2">'.$_SESSION["user"]["usr_nev"].'</div>'?>
             </div>
-            <div class="menubarButton mb-2">
-                <i class="fas fa-user-alt ms-3 me-3" style="float: left;"></i>
-                <a href="/logout" class="menubarButtonLink">Adatlapom</a>
+
+            <div class="menubarButtonContainer mb-2">
+                <div class="menubarButtonMain" id=mainAdatlapom>
+                    <i class="fas fa-fw fa-user-alt ms-4 me-3" style="float: left;"></i>
+                    <a href="" >Adatlapom</a>
+                </div>
             </div>
-            <div class="menubarButton mb-2">
-                <i class="fas fa-school ms-3 me-3" style="float: left;"></i>
-                <a href="/logout" class="menubarButtonLink">Iskola</a>
+
+            <div class="menubarButtonContainer mb-2">
+                <div class="menubarButtonMain" id=mainIskola>
+                    <i class="fas fa-fw fa-school ms-4 me-3"></i>
+                    <a >Iskola</a>
+                </div>
+                <div class="menubarButtonSubContainer" id="subIskola">
+                    <div class="menubarButtonSub">
+                        <i class="fas fa-fw fa-clipboard ms-4 me-3"></i>
+                        <a href="" >Adatlap</a>
+                    </div>
+                    <?
+                    if($_SESSION["user"]["usr_tipus"]=="vez") //Vezetőségi tag jog
+                    {
+                        echo
+                        '
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-chalkboard-teacher  ms-4 me-3"></i>
+                                <a href="" >Osztályok</a>
+                            </div>
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-book ms-4 me-3"></i>
+                                <a href="" >Tantárgyak</a>
+                            </div>
+                        ';
+                    }
+                    ?>
+                </div>
             </div>
-            <div class="menubarButton mb-2">
-                <i class="fas fa-chalkboard-teacher ms-3 me-3" style="float: left;"></i>
-                <a href="/logout" class="menubarButtonLink">Osztályom</a>
-            </div>
-            <div class="menubarButton mb-2">
-                <i class="fas fa-book-open ms-3 me-3" style="float: left;"></i>
-                <a href="/logout" class="menubarButtonLink">E-napló</a>
-            </div>
+            <?
+            if($_SESSION["user"]["usr_tipus"]=="okt")
+            {
+                echo
+                '
+                <div class="menubarButtonContainer mb-2">
+                    <div class="menubarButtonMain" id=mainIskola>
+                        <i class="fas fa-fw fa-book ms-4 me-3"></i>
+                        <a href="">Tantárgyaim</a>
+                    </div>
+                </div>
+                ';
+            }
+            if($_SESSION["user"]["usr_tipus"]=="vez")
+            {
+                echo
+                '
+                    <div class="menubarButtonContainer mb-2">
+                        <div class="menubarButtonMain" id=mainTanarok>
+                            <i class="fas fa-fw fa-chalkboard-teacher ms-4 me-3" style="float: left;"></i>
+                            <a >Tanárok</a>
+                        </div>
+                        <div class="menubarButtonSubContainer" id="subTanarok">
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-list  ms-4 me-3"></i>
+                                <a href="" >Tanárok listája</a>
+                            </div>
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-plus  ms-4 me-3"></i>
+                                <a href="" >Tanárok rögzítése</a>
+                            </div>
+                        </div>
+                    </div>
+                ';
+                echo
+                '
+                    <div class="menubarButtonContainer mb-2">
+                        <div class="menubarButtonMain" id=mainTanulok>
+                            <i class="fas fa-fw fa-graduation-cap ms-4 me-3" style="float: left;"></i>
+                            <a >Tanulók</a>
+                        </div>
+                        <div class="menubarButtonSubContainer" id="subTanulok">
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-list  ms-4 me-3"></i>
+                                <a href="" >Tanulók listája</a>
+                            </div>
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-plus  ms-4 me-3"></i>
+                                <a href="" >Tanuló rögzítése</a>
+                            </div>
+                        </div>
+                    </div>
+                ';
+                echo
+                '
+                    <div class="menubarButtonContainer mb-2">
+                        <div class="menubarButtonMain" id=mainVaezetoseg>
+                            <i class="fas fa-fw fa-street-view ms-4 me-3" style="float: left;"></i>
+                            <a >Vezetőségi tagok</a>
+                        </div>
+                        <div class="menubarButtonSubContainer" id="subVaezetoseg">
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-list  ms-4 me-3"></i>
+                                <a href="" >Vezetőségi tagok listája</a>
+                            </div>
+                            <div class="menubarButtonSub">
+                                <i class="fas fa-fw fa-plus  ms-4 me-3"></i>
+                                <a href="" >Vezetőségi tagok rögzítése</a>
+                            </div>
+                        </div>
+                    </div>
+                ';
+            }
+            if($_SESSION["user"]["usr_tipus"]=="tan" || $_SESSION["userTipusAdatok"]["okt_osztalyfonok"]>'')
+            {
+                echo '
+                    <div class="menubarButtonContainer mb-2">
+                        <div class="menubarButtonMain" id=mainOsztalyom>
+                            <i class="fas fa-fw fa-chalkboard-teacher ms-4 me-3" style="float: left;"></i>
+                            <a href="" >Osztályom</a>
+                        </div>
+                    </div>
+                    <div class="menubarButtonContainer mb-2">
+                        <div class="menubarButtonMain" id=mainEnaplo>
+                            <i class="fas fa-fw fa-book-open ms-4 me-3" style="float: left;"></i>
+                            <a href="" >E-napló</a>
+                        </div>
+                    </div>
+            ';
+            }
+            ?>
+
             <div class="menubarBottom">
                 <div class="ms-3 me-3 mb-1"style="border-top: solid 3px #4d5360;"></div>
 
-                <div class="menubarButton">
-                    <i class="fas fa-cogs ms-3 me-3" style="float: left;"></i>
-                    <a href="/logout" class="menubarButtonLink">Fiók</a>
+                <div class="menubarButtonContainer">
+                    <div class="menubarButtonMain" id=mainFiok>
+                        <i class="fas fa-fw fa-cogs ms-4 me-3" style="float: left;"></i>
+                        <a href="" >Fiók</a>
+                    </div>
                 </div>
 
-                <div class="menubarButton">
-                    <i class="fas fa-sign-out-alt ms-3 me-3" style="float: left;"></i>
-                    <a href="/logout" class="menubarButtonLink">Kijelentkezés</a>
+                <div class="menubarButtonContainer">
+                    <div class="menubarButtonMain" id=mainKijelentkezes>
+                        <i class="fas fa-fw fa-sign-out-alt ms-4 me-3" style="float: left;"></i>
+                        <a href="/logout" >Kijelentkezés</a>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-10 m-0 p-0" style="width: 87%">
+        <div class="col-10 m-0 p-0" style="">
             <div class="privSiteTop border" style="height:40px;">
                 <img class="ms-2 me-3" style="float:left; height:100%;" src="/images/siteImages/fullLogo.png" alt="Suild.hu">
                 <?echo '<div>'.$_SESSION["iskola"]["isk_nev"].'</div>';?>
